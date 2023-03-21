a49ba91387493cdf1f839d9df01d56e8b691b08b
Joey Quadros <joey_qpb@hotmail.com>
2023-03-21 18:43:25 -0300

    add reduce

--- src/app.js
+++ src/app.js
@@ -10,53 +10,59 @@ let randomHeight = Math.floor(Math.random() * (210 - 100) + 100)
 let randomAge = Math.floor(Math.random() * (100 - 18) + 18)
 let i = 0;
 
-while(i < 10){
-    const user = {
+while (i < 1) {
+  const user = {
     name: `Jhon Doe ${i}`,
     age: randomAge,
     height: `${randomHeight} cm`,
     weight: [randomWeight, randomWeight, randomWeight, randomWeight, randomWeight],
     bodyTemp: [randoBodyTemp, randoBodyTemp, randoBodyTemp, randoBodyTemp, randoBodyTemp],
-    }
-    repositoryCatraca.push(user)
-    i++
-}
-
-for(let i = 0; i<repositoryCatraca.length; i++){
-    let name =  repositoryCatraca[i].name;
-    let age =  repositoryCatraca[i].age;
-    let height =  repositoryCatraca[i].height;
-    let weightMedia = 0
-    let bodyTempMedia = 0
-
-    for(let j = 0; j < repositoryCatraca[i].weight.length; j++){
-         weightMedia = repositoryCatraca[i].weight[j] + weightMedia
-    }
-    for(let j = 0; j < repositoryCatraca[i].bodyTemp.length; j++){
-         bodyTempMedia = repositoryCatraca[i].bodyTemp[j] + bodyTempMedia
-    }
-
-    weightMedia = weightMedia/repositoryCatraca[i].weight.length
-    bodyTempMedia = bodyTempMedia/repositoryCatraca[i].bodyTemp.length
-
-    const obj = {name, age, height, weightMedia, bodyTempMedia}
-    arquivo.push(obj)
-}
-
-const dir = "./data/"
-const filename = `Catraca_${new Date(Date.now()).valueOf()}`
-const filePath = `${dir}_${filename}`;
-
-if (!fs.existsSync(dir)) {
-  fs.mkdirSync(dir);
-}
-
-fs.writeFile(filePath, JSON.stringify(arquivo),(err) => {
-  if (err) {
-    console.error(err)
-    return
   }
-  console.log('Arquivo gravado com sucesso!');
-})
+  repositoryCatraca.push(user)
+  i++
+  console.log(user)
+}
 
-export {app}
+for (let i = 0; i < repositoryCatraca.length; i++) {
+  let name = repositoryCatraca[i].name;
+  let age = repositoryCatraca[i].age;
+  let height = repositoryCatraca[i].height;
+  let weightMedia = 0
+  let bodyTempMedia = 0
+
+  for (let j = 0; j < repositoryCatraca[i].weight.length; j++) {
+    weightMedia = repositoryCatraca[i].weight[j] + weightMedia
+  }
+
+  weightMedia = weightMedia / repositoryCatraca[i].weight.length
+
+  let qtd = 1;
+  bodyTempMedia = repositoryCatraca[i].bodyTemp.reduce((acc, el, i) => {
+    qtd = i + 1
+    return acc + el
+  })
+  bodyTempMedia = bodyTempMedia / qtd
+
+  const obj = { name, age, height, weightMedia, bodyTempMedia }
+  arquivo.push(obj)
+}
+
+// const dir = "./data/"
+// const filename = `Catraca_${new Date(Date.now()).valueOf()}`
+// const filePath = `${dir}_${filename}`;
+
+// if (!fs.existsSync(dir)) {
+//   fs.mkdirSync(dir);
+// }
+
+// fs.writeFile(filePath, JSON.stringify(arquivo), (err) => {
+//   if (err) {
+//     console.error(err)
+//     return
+//   }
+//   console.log('Arquivo gravado com sucesso!');
+// })
+
+console.log(arquivo)
+
+export { app }
